2249c5f7a03a815b77ea5a6efb531387963285f9
Xavier135 <142611540+Xavier135@users.noreply.github.com>
2023-08-25 11:48:17 -0600

    Update Lista.c

--- Lista.c
+++ Lista.c
@@ -26,7 +26,37 @@ void imprimirLista(Nodo *lista);
 
 int main(){
 	
+	Nodo *lista = NULL;
 	
+	Alumno *alumno1 = crearAlumno("America America", 100, 5);
+	Alumno *alumno2 = crearAlumno("Armando Miranda", 90, 4);
+	Alumno *alumno3 = crearAlumno("Xavy Dzib", 110, 6);
+	Alumno *alumno4 = crearAlumno("Cody Pinguino", 70, 3);
+	Alumno *alumno5 = crearAlumno("Pepe Pollo", 80, 1);
+	
+	insertarNodoOrdenadoCreditos(&lista, crearNodo(alumno1));
+	insertarNodoOrdenadoCreditos(&lista, crearNodo(alumno2));
+	insertarNodoOrdenadoCreditos(&lista, crearNodo(alumno3));
+	insertarNodoOrdenadoCreditos(&lista, crearNodo(alumno4));
+	insertarNodoOrdenadoCreditos(&lista, crearNodo(alumno5));
+	
+	imprimirLista(lista);
 	
 	return 0;
 }
+
+Alumno *crearAlumno(char nombre[], int creditos, int semestre) {
+	Alumno *alumno = (Alumno *)malloc(sizeof(Alumno));
+	strcpy(alumno->nombre, nombre);
+	alumno->creditosAprobados = creditos;
+	alumno->semestreEquivalente = semestre;
+	return alumno;
+}
+
+Nodo *crearNodo(Alumno *alumno) {
+	Nodo *nodo = (Nodo *)malloc(sizeof(Nodo));
+	nodo->dato = *alumno;
+	nodo->siguiente = NULL;
+	return nodo;
+}
+
