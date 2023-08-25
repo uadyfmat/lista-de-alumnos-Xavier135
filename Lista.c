c9404a0956cbd2aca2d314e5d422f4cd6354102e
Xavier135 <142611540+Xavier135@users.noreply.github.com>
2023-08-25 11:40:02 -0600

    Add files via upload

---
+++ Lista.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct Alumno {
+	char nombre[100];
+	int creditosAprobados;
+	int semestreEquivalente;
+};
+
+typedef struct Alumno Alumno;
+
+struct Nodo {
+	Alumno dato;
+	struct Nodo *siguiente;
+};
+
+typedef struct Nodo Nodo;
+
+// Prototipos de funciones
+Alumno *crearAlumno(char nombre[], int creditos, int semestre);
+void imprimirAlumno(Alumno *alumno);
+Nodo *crearNodo(Alumno *alumno);
+int insertarNodoOrdenadoCreditos(Nodo **lista, Nodo *nodo);
+void imprimirLista(Nodo *lista);
+
+int main(){
+	
+	
+	
+	return 0;
+}
